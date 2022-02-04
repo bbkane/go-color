4e172605f12696e4315ded085634104cc7c0f8d8
Benjamin Kane <bbk1524@gmail.com>
2022-02-03 22:38:20 -0800

    factour out stdout

--- color_windows.go
+++ color_windows.go
@@ -7,15 +7,21 @@ import (
 )
 
 // Enable enables color printing on Windows and is a no-op
-// on non-Windows platforms (which enable color printing by default)
+// on non-Windows platforms (which enable color printing by default).
+// Setting for os.Stdout seems to work for stderr too?
 func Enable() error {
+	stdoutPtr := os.Stdout.Fd()
+	return enable(stdoutPtr)
+}
+
+func enable(out uintptr) error {
 	// Code adapted from: https://github.com/jedib0t/go-pretty
 
 	// See https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences?redirectedfrom=MSDN#text-formatting
 	// and
 	// https://docs.microsoft.com/en-us/windows/console/setconsolemode
 
-	handle := windows.Handle(os.Stdout.Fd())
+	handle := windows.Handle(out)
 
 	var stdoutConsoleMode uint32
 	var flags uint32 = windows.ENABLE_PROCESSED_OUTPUT |
